299684db4f2f8780bb812565501bab3fb86f0003
djessy212 <djessyfarm@gmail.com>
2025-09-14 22:57:06 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,2 +1,7 @@
 /// <reference path="..." />
 ["declare function insertRow(row: RowElement): number", "declare function deleteRow(rowId: RowID): void", "declare function updateRow("]
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): RowID;
+export function updateRow(rowId: RowID, row: RowElement): void;
+export function deleteRow(rowId: RowID): void;
