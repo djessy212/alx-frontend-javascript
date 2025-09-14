e610ad5dde9b7c391d1002417b8499b997dbb743
djessy212 <djessyfarm@gmail.com>
2025-09-14 22:52:13 +0100

    Update interface.ts

--- 0x04-TypeScript/task_3/js/interface.ts
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -1,1 +1,11 @@
-/interface.ts
+// src/interfaces.ts
+["type RowID", "number"]
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;
+}
+
+export interface RowWithID extends RowElement {
+  id: number;
+}
